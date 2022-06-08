7ae4d3d5dc4a6d5d53a614f24e80cea06f081131
Alin Voinea <contact@avoinea.com>
2022-06-08 11:15:32 +0300

    [JENKINS] Fix cypress

--- cypress/integration/blocks-divider.js
+++ cypress/integration/blocks-divider.js
@@ -20,7 +20,7 @@ describe('Blocks Tests', () => {
     // Add block
     cy.get('.ui.basic.icon.button.block-add-button').first().click();
     cy.get('.blocks-chooser .title').contains('Common').click();
-    cy.get('.ui.basic.icon.button.splitter').contains('Divider').click();
+    cy.get('.ui.basic.icon.button.dividerBlock').contains('Divider').click();
 
     // Save
     cy.get('#toolbar-save').click();
