371bb7871078ad1e1c15910ad0612d5394e29cdc
andreiggr <andrei.ggr@gmail.com>
2023-04-05 17:00:06 +0300

    fix short divider has parent

--- src/components/Divider/View.jsx
+++ src/components/Divider/View.jsx
@@ -3,15 +3,34 @@ import cx from 'classnames';
 import { Divider } from 'semantic-ui-react';
 import './divider.less';
 
-export default ({ data }) => (
-  <Divider
-    hidden={data.hidden}
-    section={data.section}
-    fitted={data.fitted}
-    className={cx(data.styles?.theme, data.short ? 'short' : '')}
-    horizontal={!!data.text}
-    inverted={data.styles?.inverted}
-  >
-    {data.text ? <div>{data.text}</div> : ''}
-  </Divider>
-);
+export default ({ data }) => {
+  return (
+    <>
+      {!data?.styles || Object.keys(data.styles).length === 0 ? (
+        <div className="styled-dividerBlock">
+          <Divider
+            hidden={data.hidden}
+            section={data.section}
+            fitted={data.fitted}
+            className={cx(data.styles?.theme, data.short ? 'short' : '')}
+            horizontal={!!data.text}
+            inverted={data.styles?.inverted}
+          >
+            {data.text ? <div>{data.text}</div> : ''}
+          </Divider>
+        </div>
+      ) : (
+        <Divider
+          hidden={data.hidden}
+          section={data.section}
+          fitted={data.fitted}
+          className={cx(data.styles?.theme, data.short ? 'short' : '')}
+          horizontal={!!data.text}
+          inverted={data.styles?.inverted}
+        >
+          {data.text ? <div>{data.text}</div> : ''}
+        </Divider>
+      )}
+    </>
+  );
+};
