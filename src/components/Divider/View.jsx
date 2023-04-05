6e24ccc934e54d6b1fc64ef514aaec7496f9c71d
David Ichim <ichim.david@gmail.com>
2023-04-05 18:20:21 +0300

    change(divider): avoid duplication and check for theme on styles for adding of parent block
    - for some reason you can enable the inverted toggle which even if you disable it, the styles
      object will be populated with { inverted: true } but block style will not add the extra div
      unless there is another value

--- src/components/Divider/View.jsx
+++ src/components/Divider/View.jsx
@@ -4,32 +4,27 @@ import { Divider } from 'semantic-ui-react';
 import './divider.less';
 
 export default ({ data }) => {
+  const DividerRender = ({ data }) => (
+    <Divider
+      hidden={data.hidden}
+      section={data.section}
+      fitted={data.fitted}
+      className={cx(data.styles?.theme, data.short ? 'short' : '')}
+      horizontal={!!data.text}
+      inverted={data.styles?.inverted}
+    >
+      {data.text ? <div>{data.text}</div> : ''}
+    </Divider>
+  );
+
   return (
     <>
-      {!data?.styles || Object.keys(data.styles).length === 0 ? (
+      {!data?.styles?.theme ? (
         <div className="styled-dividerBlock">
-          <Divider
-            hidden={data.hidden}
-            section={data.section}
-            fitted={data.fitted}
-            className={cx(data.styles?.theme, data.short ? 'short' : '')}
-            horizontal={!!data.text}
-            inverted={data.styles?.inverted}
-          >
-            {data.text ? <div>{data.text}</div> : ''}
-          </Divider>
+          <DividerRender data={data} />
         </div>
       ) : (
-        <Divider
-          hidden={data.hidden}
-          section={data.section}
-          fitted={data.fitted}
-          className={cx(data.styles?.theme, data.short ? 'short' : '')}
-          horizontal={!!data.text}
-          inverted={data.styles?.inverted}
-        >
-          {data.text ? <div>{data.text}</div> : ''}
-        </Divider>
+        <DividerRender data={data} />
       )}
     </>
   );
